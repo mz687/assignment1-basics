56755189b481715359eb1b478450360986c1d476
Mingkai Zheng <mzheng@login39.chn.perlmutter.nersc.gov>
2026-03-02 22:17:52 -0800

    finished gradient clipping

--- module/optimizer/grad_clipping.py
+++ module/optimizer/grad_clipping.py
@@ -1,13 +1,22 @@
 import torch
 import math
+from collections.abc import Iterable
 
-def grad_clip(params: list[torch.nn.Parameter],
+def grad_clip(params: Iterable[torch.nn.Parameter],
         max_l2_norm: float,
         eps: float = 1e-6):
-    for p in params:
-        if p.grad is None:
-            continue
-        
-        norm = torch.linalg.norm(p.grad)
-        if norm > max_l2_norm:
+    '''
+    Global-norm clipping: norm is computed based on all grad's norm 
+    norm = \sqrt(\sum_i(||g_i||_2^2))
+    '''
+    norm = math.sqrt(
+        sum([
+            math.pow(torch.linalg.norm(p.grad), 2) 
+            for p in params if p.grad is not None
+        ])
+    )
+    if norm > max_l2_norm:
+        for p in params:
+            if p.grad is None:
+                continue
             p.grad.mul_(max_l2_norm / (norm + eps))
